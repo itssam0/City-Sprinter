e9a35dd255cd1b07aa7158e03c58e7568750182f
Samuel Acosta Aristizábal <sacostaa1@eafit.edu.co>
2024-05-01 19:40:59 -0500

    Update .gitignore for Unity

---
+++ CitySprinters/Assets/Scripts/Events.cs
@@ -0,0 +1,16 @@
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class Event : MonoBehaviour
+{
+    public void ReplayGame()
+    {
+        SceneManager.LoadScene("Level");
+    }    
+
+    public void QuitGame()
+    {
+        Application.Quit(); 
+    }
+
+}
